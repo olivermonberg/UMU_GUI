23c1aabe8d1fddf18f50365d5e3316fe58d43876
Oliver Monberg <au554208@uni.au.dk>
2018-10-09 15:09:38 +0200

    damn convertermappe

--- UMU_GUI/Model/UserAccount.cs
+++ UMU_GUI/Model/UserAccount.cs
@@ -8,7 +8,7 @@ namespace UMU_GUI.Model
 {
     public class UserAccount
     {
-        public string UserName { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
         
       
--- UMU_GUI/ViewModel/LoginViewModel.cs
+++ UMU_GUI/ViewModel/LoginViewModel.cs
@@ -5,11 +5,35 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using MvvmFoundation.Wpf;
 
 namespace UMU_GUI.ViewModel
 {
-    public class LoginViewModel
+    public class LoginViewModel : ViewModelBase
     {
-        
+        public LoginViewModel()
+        {
+
+        }
+
+        ICommand _loginCommand;
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return _loginCommand ?? (_loginCommand = new RelayCommand(Login, CanLogin()));
+            }
+        }
+
+        public void Login()
+        {
+
+        }
+
+        public bool CanLogin(string email, string password)
+        {
+            
+        }
     }
 }
