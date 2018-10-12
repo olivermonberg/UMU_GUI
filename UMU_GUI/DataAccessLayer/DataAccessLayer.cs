88ccf6a9fd4f6136f03014dc63e09bdcbcdc5517
Oliver Monberg <au554208@uni.au.dk>
2018-10-12 12:52:03 +0200

    fix

--- UMU_GUI/DataAccessLayer/DataAccessLayer.cs
+++ UMU_GUI/DataAccessLayer/DataAccessLayer.cs
@@ -12,22 +12,31 @@ namespace UMU_GUI.DataAccessLayer
         bool Check_if_Email_and_Password_is_in_database(string Email, string Password);
         bool Check_In_Database_If_Username_And_Email_Is_Already_In_Use(string Username,string email);
         void Create_Account_In_Database(string Username, string Email, string Password);
+        bool Check_In_Database_If_Username_Is_Already_In_Use(string username);
     }
     public class DataAccessLayer : IDataAccessLayer
     {
         public bool Check_if_Email_and_Password_is_in_database(string Email, string Password)
         {
+            //Do something
             return true;
         }
 
         public bool Check_In_Database_If_Username_And_Email_Is_Already_In_Use(string Username, string email)
         {
+            //Do something
             return false;
         }
 
         public void Create_Account_In_Database(string Username, string Email, string Password)
         {
+            //Do something
+        }
 
+        public bool Check_In_Database_If_Username_Is_Already_In_Use(string username)
+        {
+            //Do something
+            return false;
         }
     }
 }
